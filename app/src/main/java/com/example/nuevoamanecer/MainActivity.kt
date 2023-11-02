6b9e55c575742bddc4dc9b4d859b6fc1f7a26f6f
jorge-nunez-gurrola <a00833455@tec.mx>
2023-11-01 19:09:59 -0600

    push branch

--- app/src/main/java/com/example/nuevoamanecer/MainActivity.kt
+++ app/src/main/java/com/example/nuevoamanecer/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column {
         Text(
-            text = "Push desde mi branch",
+            text = "Push desde mi branch, otra vez",
             modifier = modifier
         )
     }
