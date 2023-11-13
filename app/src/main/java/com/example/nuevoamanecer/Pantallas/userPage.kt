644cdda92dafa396a1a2c1511eeaf29320070f76
jorge-nunez-gurrola <a00833455@tec.mx>
2023-11-13 17:47:00 -0600

    Pagina del usuario actualizada con NavController

--- app/src/main/java/com/example/nuevoamanecer/Pantallas/userPage.kt
+++ app/src/main/java/com/example/nuevoamanecer/Pantallas/userPage.kt
@@ -1,56 +1,80 @@
 package com.example.nuevoamanecer.Pantallas
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.nuevoamanecer.R
 
 @Composable
 fun userPage(navController: NavHostController){
+
+    Box(
+        modifier = Modifier.background(Color(0xFFa1beff))
+
+    ){
+
+    }
+
     Column {
-        Text(
+
+        FloatingActionButton(
+            onClick = {
+                navController.navigate("Login")
+            },
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(vertical = 10.dp),
-            text = "Pagina del Usuario"
-        )
+                .align(Alignment.End)
+        ) {
+            Icon(Icons.Filled.Close, "Cerrar Sesion")
+        }
+
         Row(
             modifier = Modifier
                 .padding(vertical = 10.dp)
                 .align(Alignment.CenterHorizontally)
         ) {
-            Button(
+            FloatingActionButton(
                 onClick = {
                     navController.navigate("Tablero")
                 },
                 modifier = Modifier.padding(vertical = 10.dp, horizontal = 10.dp)
             ) {
-                Text(text = "Tablero")
+                Image(
+                    painter = painterResource(id = R.drawable.tableroimage),
+                    contentDescription = "Tablero",
+                    modifier = Modifier.size(220.dp)
+                )
             }
-            Button(
+            FloatingActionButton(
                 onClick = {
                     navController.navigate("Juegos")
                 },
                 modifier = Modifier.padding(vertical = 10.dp)
             ) {
-                Text(text = "Juegos")
+                Image(
+                    painter = painterResource(id = R.drawable.juegosimage),
+                    contentDescription = "Juegos",
+                    modifier = Modifier.size(220.dp)
+                )
             }
         }
-        Button(
-            onClick = {
-                navController.navigate("Login")
-            },
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(vertical = 30.dp)
-        ) {
-            Text(text = "Ir a Login")
-        }
+
+
     }
 }
