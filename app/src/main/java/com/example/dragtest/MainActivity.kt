c90830db6af2a717520f7eac3019e0a85ab4ae24
Gabriel Maynez <A01236355@tec.mx>
2023-11-15 18:27:27 -0600

    juego v2

--- app/src/main/java/com/example/dragtest/MainActivity.kt
+++ app/src/main/java/com/example/dragtest/MainActivity.kt
@@ -10,14 +10,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
@@ -29,6 +32,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dragtest.ui.theme.DragTestTheme
+import kotlinx.coroutines.delay
 import kotlin.random.Random
 import kotlin.math.abs
 
@@ -50,6 +54,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val backgroundPainter = painterResource(id = R.drawable.blackboard_background)
+                    Image(
+                        painter = backgroundPainter,
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize()
+                    )
                     Box() {
                         val d = LocalDensity.current
                         val maxX = LocalConfiguration.current.screenWidthDp.dp
@@ -78,6 +88,11 @@ class MainActivity : ComponentActivity() {
                         val image6: Painter = painterResource(id = R.drawable.pentagono_contorno)
                         val image7: Painter = painterResource(id = R.drawable.tirangulo_relleno)
                         val image8: Painter = painterResource(id = R.drawable.triangulo_contorno)
+                        var snapped1to2 by remember { mutableStateOf(false) }
+                        var snapped3to4 by remember { mutableStateOf(false) }
+                        var snapped5to6 by remember { mutableStateOf(false) }
+                        var snapped7to8 by remember { mutableStateOf(false) }
+                        var showWinMessage by remember { mutableStateOf(false) }
                         val snapDistance = 50f
 
                         Image(
@@ -94,6 +109,7 @@ class MainActivity : ComponentActivity() {
                                         if (abs(offsetX1 - offsetX2) < snapDistance && abs(offsetY1 - offsetY2) < snapDistance) {
                                             offsetX1 = offsetX2
                                             offsetY1 = offsetY2
+                                            snapped1to2 = true
                                         }
                                     }
                                 }
@@ -113,6 +129,7 @@ class MainActivity : ComponentActivity() {
                                         if (abs(offsetX2 - offsetX1) < snapDistance && abs(offsetY2 - offsetY1) < snapDistance) {
                                             offsetX2 = offsetX1
                                             offsetY2 = offsetY1
+                                            snapped1to2 = true
                                         }
                                     }
                                 }
@@ -132,6 +149,7 @@ class MainActivity : ComponentActivity() {
                                         if (abs(offsetX3 - offsetX4) < snapDistance && abs(offsetY3 - offsetY4) < snapDistance) {
                                             offsetX3 = offsetX4
                                             offsetY3 = offsetY4
+                                            snapped3to4 = true
                                         }
                                     }
                                 }
@@ -151,6 +169,7 @@ class MainActivity : ComponentActivity() {
                                         if (abs(offsetX4 - offsetX3) < snapDistance && abs(offsetY4 - offsetY3) < snapDistance) {
                                             offsetX4 = offsetX3
                                             offsetY4 = offsetY3
+                                            snapped3to4 = true
                                         }
                                     }
                                 }
@@ -169,6 +188,7 @@ class MainActivity : ComponentActivity() {
                                         if (abs(offsetX5 - offsetX6) < snapDistance && abs(offsetY5 - offsetY6) < snapDistance) {
                                             offsetX5 = offsetX6
                                             offsetY5 = offsetY6
+                                            snapped5to6 = true
                                         }
                                     }
                                 }
@@ -187,6 +207,7 @@ class MainActivity : ComponentActivity() {
                                         if (abs(offsetX6 - offsetX5) < snapDistance && abs(offsetY6 - offsetY5) < snapDistance) {
                                             offsetX6 = offsetX5
                                             offsetY6 = offsetY5
+                                            snapped5to6 = true
                                         }
                                     }
                                 }
@@ -205,6 +226,7 @@ class MainActivity : ComponentActivity() {
                                         if (abs(offsetX7 - offsetX8) < snapDistance && abs(offsetY7 - offsetY8) < snapDistance) {
                                             offsetX7 = offsetX8
                                             offsetY7 = offsetY8
+                                            snapped7to8 = true
                                         }
                                     }
                                 }
@@ -223,13 +245,37 @@ class MainActivity : ComponentActivity() {
                                         if (abs(offsetX8 - offsetX7) < snapDistance && abs(offsetY8 - offsetY7) < snapDistance) {
                                             offsetX8 = offsetX7
                                             offsetY8 = offsetY7
+                                            snapped7to8 = true
                                         }
                                     }
                                 }
                         )
+                        if (snapped1to2 && snapped3to4 && snapped5to6 && snapped7to8) {
+                            showWinMessage = true
+                            LaunchedEffect(Unit) {
+                                delay(2000L) // Show message for 2 seconds
+                                showWinMessage = false
+                            }
+                        }
+
+                        if (showWinMessage) {
+                            WinMessage()
+                        }
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun WinMessage() {
+    Text(
+        text = "Ganaste!",
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+            .background(Color.Black.copy(alpha = 0.5f)),
+        color = Color.White
+    )
 }
