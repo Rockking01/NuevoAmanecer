5fb03ee10a21cff59258bd87a5dd2416430ed42e
jorge-nunez-gurrola <a00833455@tec.mx>
2023-11-22 19:31:59 -0600

    no importante

---
+++ app/src/main/java/com/example/nuevoamanecer/Pantallas/photoData.kt
@@ -0,0 +1,34 @@
+package com.example.nuevoamanecer.Pantallas
+
+import com.example.nuevoamanecer.R
+
+
+fun getPhoto(): List<Int> {
+
+    return listOf(
+
+        (R.drawable.abeja),
+        (R.drawable.chango),
+        (R.drawable.gato),
+        (R.drawable.perro),
+        (R.drawable.pato),
+        (R.drawable.pez),
+        (R.drawable.oso),
+        (R.drawable.mariposa),
+        (R.drawable.pulpo),
+        (R.drawable.rana),
+        (R.drawable.vaca),
+        (R.drawable.pollito),
+        (R.drawable.jirafa),
+        (R.drawable.panda),
+        (R.drawable.elefante),
+        (R.drawable.conejo),
+        (R.drawable.cerdo),
+        (R.drawable.ballena),
+        (R.drawable.loro),
+        (R.drawable.cabra),
+
+
+    )
+}
+
--- app/src/main/java/com/example/nuevoamanecer/Pantallas/tableroPage.kt
+++ app/src/main/java/com/example/nuevoamanecer/Pantallas/tableroPage.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
